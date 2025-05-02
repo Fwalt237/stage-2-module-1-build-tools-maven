2882bd66e76537ef9cb2c11b52ff472f44290506
Fwalt237 <patrickkamwa17@gmail.com>
2025-05-02 22:03:11 +0100

    maven_task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return isNumeric(str) && Double.parseDouble(str) > 0;
+    }
+    private static boolean isNumeric(String str) {
+        return str != null && str.matches("-?\\d+(\\.\\d+)?");
     }
 }
